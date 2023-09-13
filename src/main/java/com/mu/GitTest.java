456a4aca5e926847c6cf307a373c04fcaff54413
july <3514736731@qq.com>
2023-09-13 16:20:11 +0800

    第一个提交

---
+++ src/main/java/com/mu/GitTest.java
@@ -0,0 +1,12 @@
+package com.mu;
+
+/**
+ * @author July
+ * @description:
+ * @date 2023/9/13 16:13
+ */
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("你好");
+	}
+}
