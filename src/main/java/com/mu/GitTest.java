6b74ea16ac9e3244bb6156df856fddbff1cda41a
july <3514736731@qq.com>
2023-09-13 16:29:32 +0800

    第一个提交

--- src/main/java/com/mu/GitTest.java
+++ src/main/java/com/mu/GitTest.java
@@ -7,6 +7,7 @@ package com.mu;
  */
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("你好");
+		System.out.println("你好1");
+		System.out.println("你好2");
 	}
 }
